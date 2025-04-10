2127840cb9eec75d361567c30ba46979ca671e4e
Gabe Meier <gabemeier@Gabes-MacBook-Pro.local>
2025-04-10 14:17:53 -0500

    Add functionality for a basic query

---
+++ dbTesting.py
@@ -0,0 +1,28 @@
+import pymysql
+import creds 
+
+def mysqlconnect():
+    # To connect MySQL database
+    conn = pymysql.connect(
+        host= creds.host,
+        user= creds.user, 
+        password = creds.rds_password,
+        db=creds.db,
+        )
+    cur = conn.cursor()
+
+    # Execute Query
+    cur.execute("""SELECT name FROM city
+                Limit 5""")
+    output = cur.fetchall()
+    
+    # Print Results
+    for row in output:
+        print(row[0], "\t", row[1], "\t", row[2], "\t", row[3], "\t", row[4])
+      
+    # To close the connection
+    conn.close()
+  
+# Driver Code
+if __name__ == "__main__" :
+    mysqlconnect()
--- flaskapp.py
+++ flaskapp.py
@@ -1,1 +1,48 @@
-print("hello world")
+from flask import Flask
+from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+import pymysql
+import creds 
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def hello():
+    return '<h2>Hello from Flask!</h2>'
+
+def get_conn():
+    conn = pymysql.connect(
+        host= creds.host,
+        user= creds.user, 
+        password = creds.rds_password,
+        db=creds.db,
+        )
+    return conn
+
+def execute_query(query, args=()):
+    cur = get_conn().cursor()
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+def display_html(rows):
+    html = ""
+    html += """<table><tr><th>Name</th></tr>"""
+
+    for r in rows:
+        html += "<tr><td>" + str(r[0]) + "</td><td>"
+    html += "</table></body>"
+    return html
+
+@app.route("/viewdb")
+def viewdb():
+    rows = execute_query("""SELECT name FROM city
+                Limit 5""")
+    return display_html(rows)
+
+# these two lines of code should always be the last in the file
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
